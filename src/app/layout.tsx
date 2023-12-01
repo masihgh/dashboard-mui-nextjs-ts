12a46031e605068a30f2ae1bb30a480b8c49a639
masihgh <masihprogrammer@gmail.com>
2023-12-01 15:51:23 +0330

    huh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,35 @@
 // components/Layout.tsx
-import React from 'react';
-import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
-import AppBarMenu from '@/components/AppBarMenu/AppBarMenu';
-import Footer from '@/components/Footer/Footer';
-import { Box, Grid } from '@mui/material';
-import Sidebar from '@/components/Sidebar/Sidebar';
+import React from "react";
+import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
+import AppBarMenu from "@/components/AppBarMenu/AppBarMenu";
+import Footer from "@/components/Footer/Footer";
+import { Box, Grid } from "@mui/material";
+import Sidebar from "@/components/Sidebar/Sidebar";
 
 export const metadata = {
-  title: 'Dashboard',
-  description: 'Dashboard for all',
+  title: "Dashboard",
+  description: "Dashboard for all",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
         <ThemeRegistry>
-          <AppBarMenu />
-          <Grid container>
-            <Grid item xs={12} sm={3}>
-              <Sidebar />
+            <AppBarMenu />
+            <Grid container>
+              <Grid item xs={12} sm={3}>
+                <Sidebar />
+              </Grid>
+              <Grid item xs={12} sm={9}>
+                {children}
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={9}>
-              {children}
-            </Grid>
-          </Grid>
-          <Footer />
+            <Footer />
         </ThemeRegistry>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 
 export default function HomePage() {
   return (
+    
     <Box sx={{ display: 'flex' }}>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel atque sapiente inventore perspiciatis totam aliquid corporis, id in earum illo illum autem ad, perferendis similique repellat modi sunt, quis consectetur!</p>
     </Box>
